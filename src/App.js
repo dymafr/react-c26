0db2c6caa8f007bd19f9d6123713cf6cfbb88cb9
dymafr <contact@dyma.fr>
2022-11-16 14:08:10 +0100

    l4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import styles from './App.module.scss';
-import Fade from './Fade';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
 
 function App() {
   const [show, setShow] = useState(true);
+  const refA = useRef(null);
+  const refB = useRef(null);
+  const ref = show ? refA : refB;
 
   return (
     <div
       className={`d-flex flex-column p-20 align-items-center ${styles.appContainer}`}
     >
       <button onClick={() => setShow(!show)} className="btn btn-primary mb-20">
-        Toggle
+        Switch
       </button>
-      <Fade in={show}>
-        <h1>Bonjour</h1>
-      </Fade>
+      <SwitchTransition mode="in-out">
+        <CSSTransition
+          key={show ? '1' : '2'}
+          nodeRef={ref}
+          classNames={styles}
+          timeout={1000}
+        >
+          <div ref={ref}>
+            {show && <h1>A</h1>}
+            {!show && <h1>B</h1>}
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 }
